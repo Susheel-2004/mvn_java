c891a905b765499e8b35196d7e238d6aacff094e
Susheel-2004 <susheel.tatikonda@gmail.com>
2024-11-25 09:59:02 +0530

    intial commit

---
+++ src/main/java/com/app/demo/mvn_java/App.java
@@ -0,0 +1,14 @@
+package com.app.demo.mvn_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hellow world");
+    }
+}
